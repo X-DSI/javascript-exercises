f0566214934b80850b86424373284ddb44cbc495
X-DSI <xmandsi@gmail.com>
2022-06-21 10:44:29 +0530

    03 Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  // using built-in functions
+  //   return word.split("").reverse().join("");
+  // using for-loop
 
+  let rword = "";
+  for (let index = word.length - 1; index >= 0; index--) {
+    rword += word[index];
+  }
+
+  return rword;
 };
 
 // Do not edit below this line
