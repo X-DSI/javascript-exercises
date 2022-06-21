47b9d09be7ab2f29e93bc50c1193f1d62476effb
X-DSI <xmandsi@gmail.com>
2022-06-21 10:23:55 +0530

    02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  let rword = "";
+  if (num >= 0) {
+    while (num > 0) {
+      rword += word;
+      num--;
+    }
+    return rword;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
