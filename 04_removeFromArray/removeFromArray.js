1ac6c3512248e796d4a1ba81aacde35c4b97b940
X-DSI <xmandsi@gmail.com>
2022-06-23 11:06:26 +0530

    04 in Progress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function ([a, b, c, d], ...args) {};
+const removeFromArray = function ([a, b, c, d], e) {
+  const array = [a, b, c, d];
+
+  if (e === 3) {
+    array.splice(e);
+
+    return array;
+  }
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
