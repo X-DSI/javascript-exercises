fd7fb257eb5f66cffe1d658467d4d46577cca073
X-DSI <xmandsi@gmail.com>
2022-07-06 11:46:27 +0530

    05 Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,12 +3,26 @@ const sumAll = function (...args) {
   const finalno = args[1];
   let sum = baseno;
   let nextno = 0;
-  do {
-    nextno = baseno + 1;
-    sum = sum + nextno;
-    baseno += 1;
-  } while (nextno < finalno);
-  return sum;
+
+  if (!Number.isInteger(baseno) || !Number.isInteger(finalno)) return "ERROR";
+
+  if (baseno < 0 || finalno < 0) return "ERROR";
+
+  if (baseno < finalno) {
+    do {
+      nextno = baseno + 1;
+      sum = sum + nextno;
+      baseno += 1;
+    } while (nextno < finalno);
+    return sum;
+  } else if (baseno > finalno) {
+    do {
+      nextno = baseno - 1;
+      sum = sum + nextno;
+      baseno -= 1;
+    } while (nextno > finalno);
+    return sum;
+  }
 };
 // Do not edit below this line
 module.exports = sumAll;
