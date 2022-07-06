be9f63166a010e84987ebdb3af030d91a42d9a3a
X-DSI <xmandsi@gmail.com>
2022-07-06 10:45:31 +0530

    05 Tests 1,2 Passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+  let baseno = args[0];
+  const finalno = args[1];
+  let sum = baseno;
+  let nextno = 0;
+  do {
+    nextno = baseno + 1;
+    sum = sum + nextno;
+    baseno += 1;
+  } while (nextno < finalno);
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
