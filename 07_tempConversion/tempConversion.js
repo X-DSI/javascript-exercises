adc77496ec0b970c64fa67e6af4754618ca3df75
X-DSI <xmandsi@gmail.com>
2022-07-07 15:14:36 +0530

    07 Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 // formula for F to C => 	[°C] = ([°F] − 32) × 5⁄9
 const ftoc = function (ftemp) {
-  let ctemp = ((ftemp - 32) * 5) / 9;
-  return ctemp;
+  let ctemp = (ftemp - 32) * (5 / 9);
+  return Math.round(ctemp * 10) / 10;
 };
 
 // formula for C to F => [°F] = [°C] × 9⁄5 + 32
 const ctof = function (ctemp) {
-  let ftemp = (ctemp * 5) / 9 + 32;
-  return ftemp;
+  let ftemp = ctemp * (9 / 5) + 32;
+  return Math.round(ftemp * 10) / 10;
 };
 
 // Do not edit below this line
