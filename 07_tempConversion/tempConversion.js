7f2ef39dcfc21e60cff2d93acd94e5cfe5e96e99
X-DSI <xmandsi@gmail.com>
2022-07-07 13:04:30 +0530

    0

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+// formula for F to C => 	[°C] = ([°F] − 32) × 5⁄9
+const ftoc = function (ftemp) {
+  let ctemp = ((ftemp - 32) * 5) / 9;
+  return ctemp;
 };
 
-const ctof = function() {
-
+// formula for C to F => [°F] = [°C] × 9⁄5 + 32
+const ctof = function (ctemp) {
+  let ftemp = (ctemp * 5) / 9 + 32;
+  return ftemp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
