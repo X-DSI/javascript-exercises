1a45c16ca134421ca77c8c521e80562db8346a40
X-DSI <xmandsi@gmail.com>
2022-07-08 11:00:57 +0530

    09 1 test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  // works with single words and words without punctuation
+  //   return s == s.split("").reverse().join("") ? true : false;
+  // How to remove space n punctuation and combine the whole thing into a single string
 };
 
 // Do not edit below this line
