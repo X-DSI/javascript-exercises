d44d2214668d1a5b4ade0a7385b165ef13be3ae6
X-DSI <xmandsi@gmail.com>
2022-07-08 11:42:28 +0530

    09 90% Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,12 @@
 const palindromes = function (str) {
   // works with single words and words without punctuation
-  //   return s == s.split("").reverse().join("") ? true : false;
+  //   return s.split("").reverse().join("") ? true : false;
   // How to remove space n punctuation and combine the whole thing into a single string
   let tempstr = str.replace(/[^a-zA-Z0-9]/g, "");
   let tempstr1 = tempstr.replace(/\s+/g, "");
   let finalstr = tempstr1.toLowerCase();
-
-  return (finalstr = finalstr.split("").reverse().join("") ? true : false);
+  console.log(finalstr);
+  return finalstr.split("").reverse().join("") ? true : false;
 };
 
 // Do not edit below this line
