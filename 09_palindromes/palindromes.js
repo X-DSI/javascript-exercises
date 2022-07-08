9c32036580fd7578d03d4151abef43c2165523cc
X-DSI <xmandsi@gmail.com>
2022-07-08 11:49:01 +0530

    09 Finally Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -5,8 +5,10 @@ const palindromes = function (str) {
   let tempstr = str.replace(/[^a-zA-Z0-9]/g, "");
   let tempstr1 = tempstr.replace(/\s+/g, "");
   let finalstr = tempstr1.toLowerCase();
-  console.log(finalstr);
-  return finalstr.split("").reverse().join("") ? true : false;
+  //   console.log(finalstr);
+  if (finalstr === finalstr.split("").reverse().join("")) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
